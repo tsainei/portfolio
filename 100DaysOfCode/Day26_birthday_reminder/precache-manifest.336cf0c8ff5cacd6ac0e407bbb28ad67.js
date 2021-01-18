4a751ae515747df860342be4116c4173df32df96
Tsai-Nei Yuan <anita810910@gmail.com>
2021-01-18 18:39:35 +0100

    add files

---
+++ 100DaysOfCode/Day26_birthday_reminder/precache-manifest.336cf0c8ff5cacd6ac0e407bbb28ad67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448754e018a887e6f6ec5b3a75bc7afc",
+    "url": "/100DaysOfCode/Day26_birthday_reminder/index.html"
+  },
+  {
+    "revision": "2d404b902bde3694e7d7",
+    "url": "/100DaysOfCode/Day26_birthday_reminder/static/css/main.4f64ed23.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/100DaysOfCode/Day26_birthday_reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/100DaysOfCode/Day26_birthday_reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d404b902bde3694e7d7",
+    "url": "/100DaysOfCode/Day26_birthday_reminder/static/js/main.1f0a6cd4.chunk.js"
+  },
+  {
+    "revision": "e0d157855652c68873da",
+    "url": "/100DaysOfCode/Day26_birthday_reminder/static/js/runtime-main.0eeadfbe.js"
+  }
+]);
